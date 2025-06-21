236037bc41d77aa4c71379fa5d8a3af63a120a78
Balaji Varadaraj <112942350+balajibala2004@users.noreply.github.com>
2025-06-22 00:14:14 +0530

    Add files via upload

---
+++ Projects/Academic Scheduler/BackEnd/src/main/java/com/example/Regi/Smart/RegiSmartApplication.java
@@ -0,0 +1,13 @@
+package com.example.Regi.Smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegiSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegiSmartApplication.class, args);
+	}
+
+}
